b9524c350ad85e197e8c36724db47c7304a00773
muthu-PM <muthu@abc.com>
2025-09-11 05:33:44 +0530

    deployed

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const menuOpenButton = document.querySelector("#menu-open-button")
+const menuCloseButton = document.querySelector("#menu-close-button")
+const navlinks = document.querySelectorAll(".nav-menu .nav-link")
+
+menuOpenButton.addEventListener("click", ()=>{
+    document.body.classList.toggle("show-mobile-menu");
+})
+
+menuCloseButton.addEventListener("click", ()=> menuOpenButton.click())
+
+navlinks.forEach(link => {
+    link.addEventListener("click", ()=> menuOpenButton.click());
+});
+
+// Testimonial Slider 
+
+const swiper = new Swiper('.slider-wrapper', {
+   
+    loop: true,
+    spaceBetween:25,
+    grabCursor:true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // responsive breakpoints
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        },
+    }
+  
+    
+  });
